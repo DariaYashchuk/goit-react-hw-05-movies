2cb1b82341f0d5e2f03d2ecdeed0f6a8d7a1d2fa
DariaYashchuk <dariayashchuk2000@gmail.com>
2023-07-24 14:17:48 +0300

    start

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,43 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import * as MoviesService from 'services/movies-service';
 
 const Home = () => {
+  const [trendingMovies, setTrendingMovies] = useState([]);
   useEffect(() => {
-    MoviesService.getTrending();
+    async function addTrendingMovies() {
+      try {
+        const { data } = await MoviesService.getTrending();
+        console.log(data);
+
+        const formatedTrendingMovies = data.results.map(({ id, title }) => ({
+          id,
+          title,
+        }));
+
+        // setTrendingMovies(prevTrendingMovies => [
+        //   ...prevTrendingMovies,
+        //   ...formatedTrendingMovies,
+        // ]);
+        setTrendingMovies(formatedTrendingMovies);
+      } catch {
+        // alert('error');
+      } finally {
+      }
+    }
+    addTrendingMovies();
   }, []);
+
   return (
     <div>
       <h1>Trending today</h1>
-      <ul></ul>
+      <ul>
+        {trendingMovies.map(({ id, title }) => (
+          <li key={id}>
+            <Link>{title}</Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
